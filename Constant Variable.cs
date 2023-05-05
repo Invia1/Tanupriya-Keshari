349869bf9667a12e24343410399ac48a7bd03a6e
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 21:48:33 +0530

    Add files via upload

---
+++ Constant Variable.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+     const int a = 10;
+     const int b = 20;
+
+     static void Main()
+     {
+        const int c = a+b;
+        Console.WriteLine(c);
+        Console.ReadLine();
+     }
+}
