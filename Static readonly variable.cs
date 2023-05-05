8eae50fd80477e3e395688770c03faaa7ebd94d4
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 20:57:53 +0530

    Add files via upload

---
+++ Static readonly variable.cs
@@ -0,0 +1,22 @@
+using System;
+  
+class FullName {
+  
+    public static readonly string name;
+    public static readonly string surname;
+   
+    public FullName(string a, string b)
+    {
+  
+        name = a;
+        surname = b;
+        Console.WriteLine("Display value of Name :- {0} "
+                          + "Surname :- {1}", name, surname);
+    }
+  
+
+    static public void Main()
+    {
+        FullName ob = new FullName("Tanupriya", "Keshari");
+    }
+}
