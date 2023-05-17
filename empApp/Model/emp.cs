1a3528ad6d3ae1bda8b3daebea344ae38d0edaf9
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-17 09:14:22 +0530

    Add files via upload

---
+++ empApp/Model/emp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace SmallEmployeeApp.Models
+{
+    class Employee
+    {
+        // public int EId { get; set; }
+        private int _eId;
+        public int EId
+        {
+            get
+            {
+                return _eId;
+            }
+            set
+            {
+                _eId = value;
+
+            }
+        }
+
+        public string EName { get; set; }
+        public int EAge { get; set; }
+    }
+}
