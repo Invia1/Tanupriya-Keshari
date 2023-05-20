ed0766bda16c244727bd5d89b9b36acb3bf7438b
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-20 23:52:00 +0530

    Add files via upload

---
+++ FootballTeamApp/BLL/PlayersActivities.cs
@@ -0,0 +1,98 @@
+﻿using FootBallTeamApp.Functionality;
+using FootBallTeamApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace FootBallTeamApp.BLL
+{
+    class PlayersActivities: ITeamPlayersOperations
+    {
+        Players players = new Players();
+        List<Players> obj = new List<Players>();
+
+        public object ID { get; private set; }
+        public object Name { get; private set; }
+        public object Profile { get; private set; }
+
+        public void AddPlayersName(Players pl)
+        {
+           obj.Add(pl);
+        }
+        
+        public void RemovePlayersName(int ID)
+        {
+            /*for(int i = 0; i < obj.Count; i++)
+            {
+                if (ID == obj[i].ID)
+                {
+                    obj.RemoveAt(i);
+
+                }
+
+            }*/
+            var Remove = obj.Where(obj => obj.ID == ID).ToList();
+            obj.RemoveAt(ID);
+
+
+        }
+        public void UpdateList(int ID)
+        {
+            Players players = new Players();
+            for (int i = 0; i < obj.Count; i++)
+            {
+                if (ID == obj[i].ID)
+                {
+                    obj.RemoveAt(i);
+
+                    Console.WriteLine("Enter new id");
+                    players.ID = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter new name");
+                    players.Name = Console.ReadLine();
+
+                    Console.WriteLine("Enter new Profile");
+                    players.Profile = Console.ReadLine();
+
+                    obj.Add(players);
+
+                }
+
+            }
+            /*var Update = obj.Where(obj => obj.ID == ID).ToList();
+            foreach (var p in Update)
+            {
+                obj.RemoveAt((int)p.ID);
+                Console.WriteLine("Enter new id");
+                players.ID = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter new name");
+                players.Name = Console.ReadLine();
+
+                Console.WriteLine("Enter new Profile");
+                players.Profile = Console.ReadLine();
+
+                obj.Add(players);
+            }*/
+        }
+        public void SearchData(int id)
+        {
+            var Data = obj.Where(obj => obj.ID == id).ToList();
+
+            if(Data.Count > 0)
+            {
+               
+               foreach (var item in Data)
+                {
+                    Console.WriteLine(item.ID+" "+item.Name+" "+item.Profile);
+                }
+            }
+        }
+        public List<Players> GetPlayersList()
+        {
+            return obj;
+        }
+    }
+}
+
---
+++ FootballTeamApp/Functionality/ITeamPlayersOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using FootBallTeamApp.Models;
+using FootBallTeamApp.Entry_Point;
+using FootBallTeamApp.BLL;
+namespace FootBallTeamApp.Functionality
+{
+    public interface ITeamPlayersOperations
+    {
+        void AddPlayersName(Players pl);
+        void RemovePlayersName(int ID);
+        void UpdateList(int ID);
+        void SearchData(int ID);
+         List<Players> GetPlayersList();
+
+
+    }
+}
